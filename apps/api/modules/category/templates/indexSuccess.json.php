38e93afd667c57ba2c3538c1262d66734f5d5c84
Nicolas OBRE <nicolas@obre.fr>
2012-06-10 00:59:32 +0200

    Modification du json (un seul fichier) /api_dev.php/get.json + amélioration de l'indentation

--- apps/api/modules/category/templates/indexSuccess.json.php
+++ apps/api/modules/category/templates/indexSuccess.json.php
@@ -1,15 +1,24 @@
 {
-"category": <?php if(count($categorys) > 1) echo "["?>
-
+  "category": [
 <?php $nb = count($categorys); $i = 0; foreach ($categorys as $category): ++$i ?>
-  {
+    {
 <?php $nb1 = count($category); $j = 0; foreach ($category as $key => $value): ++$j ?>
-    "<?php echo $key ?>": <?php echo json_encode($value).($nb1 == $j ? '' : ',') ?>
+      "<?php echo $key ?>": <?php echo json_encode($value).($nb1 == $j ? '' : ',') ?>
  
 <?php endforeach ?>
-  }<?php echo $nb == $i ? '' : ',' ?>
+    }<?php echo $nb == $i ? '' : ',' ?>
  
 <?php endforeach ?>
-<?php if(count($categorys) > 1) echo "]"?>
-  
+  ],
+  "product": [
+<?php $nb = count($products); $i = 0; foreach ($products as $product): ++$i ?>
+    {
+<?php $nb1 = count($product); $j = 0; foreach ($product as $key => $value): ++$j ?>
+      "<?php echo $key ?>": <?php echo json_encode($value).($nb1 == $j ? '' : ',') ?>
+ 
+<?php endforeach ?>
+    }<?php echo $nb == $i ? '' : ',' ?>
+ 
+<?php endforeach ?>
+  ]
 }
