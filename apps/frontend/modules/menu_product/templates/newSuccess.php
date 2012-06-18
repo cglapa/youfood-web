2b641eefcfebc7a656ce644fd2bfdd8bf9871766
Nicolas OBRE <nicolas@obre.fr>
2012-06-18 21:26:39 +0200

    Ajout du fil d'Ariane sur la page d'ajout de plat à un menu

--- apps/frontend/modules/menu_product/templates/newSuccess.php
+++ apps/frontend/modules/menu_product/templates/newSuccess.php
@@ -1,5 +1,17 @@
+<ul class="breadcrumb">
+    <li>
+        <a href="<?php echo url_for('menu')?>">Liste des menus</a><span class="divider">></span>
+    </li>
+    <li>
+        <a href="<?php echo url_for('menu_detail', $menu)?>">Liste des plats du menu <?php echo $menu->getName() ?></a><span class="divider">></span>
+    </li>
+    <li class="active">
+        Ajouter un produit au menu <?php echo $menu->getName() ?>
+    </li>
+</ul>
+
 <div class="page-header">
-    <h1>Ajouter un produit au menu <?php echo strtolower($menu->getName()) ?></h1>
+    <h1>Ajouter un produit au menu <?php echo $menu->getName() ?></h1>
 </div>
 
 <?php include_partial('form', array('form' => $form, 'menu' => $menu)) ?>
